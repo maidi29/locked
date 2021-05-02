d0d77f63f6ca1d6eb5d1163ae03cbcd8feac52f2
mareike <mareike.haug@web.de>
2021-05-03 00:55:06 +0200

    transfer code to this repo

---
+++ Assets/Scripts/MysteryLockOpener.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MysteryLockOpener : MonoBehaviour {
+	public int keyCount;
+	public GameObject pointLight;
+	//public GameObject winLight;
+    public Material whiteSkybox;
+
+	public void OpenDoor () {
+		pointLight.SetActive (true);
+		//winLight.SetActive (true);
+		this.GetComponentInParent<Rigidbody> ().isKinematic = false;
+		this.GetComponentInParent<HingeJoint> ().useMotor = true;
+        RenderSettings.skybox = whiteSkybox;
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Locked
+
+
+[![Trailer Video](https://img.youtube.com/vi/rngUstGVDdE/0.jpg)](https://www.youtube.com/watch?v=rngUstGVDdE)
+
+
+Locked is a Virtual Reality Escape Room, where the user has to find items and solve riddles to open the door of this mysterious and sinister room.
+
+Locked was a Unity3D project created in Open Games Workshop at LMU Munich.
